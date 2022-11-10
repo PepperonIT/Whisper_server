44c6d8614870da54ceab5d90f277c20b6ad1c805
oscarlundberg95 <oscarlundberg95@hotmail.se>
2022-11-10 13:42:39 +0000

    pylint

--- src/upload.py
+++ src/upload.py
@@ -1,49 +1,56 @@
-from flask import *  
-from werkzeug.utils import secure_filename
+"""
+Upload module
+Essentially the main file, holding the server and api functionality
+"""
+
 import os
+from flask import *# pylint: disable=wildcard-import, unused-wildcard-import
+from werkzeug.utils import secure_filename# pylint: disable=unused-import
 import whisper
 import jsonify
 from pydub import AudioSegment
 import config
 
-app = Flask(__name__)  
-app.config['UPLOAD_FOLDER'] = "files"
-app.config['JSON_AS_ASCII'] = False
+APP = Flask(__name__)
+APP.config['UPLOAD_FOLDER'] = "../files"
+APP.config['JSON_AS_ASCII'] = False
 
-model = whisper.load_model("small")
+MODEL = whisper.load_model("small")
 
-@app.route('/')  
-def upload():  
-    return render_template("file_upload.html")  
- 
-@app.route('/success', methods = ['POST'])  
-def success():  
-    if request.method == 'POST':  
-        f = request.files['file']  
-        path = os.path.join(app.config['UPLOAD_FOLDER'], f.filename)
-        f.save(path)
+@APP.route('/')
+def upload():
+    """http"""
+    return render_template("file_upload.html")
+
+@APP.route('/success', methods=['POST'])
+def success():# pylint: disable=inconsistent-return-statements
+    """handling different audio files"""
+    if request.method == 'POST':
+        file = request.files['file']
+        path = os.path.join(APP.config['UPLOAD_FOLDER'], file.filename)
+        file.save(path)
         if path[:-3] == 'wav':
             AudioSegment.from_wav(path).export(path[:-3]+"mp3", format="mp3")
-            result = model.transcribe(path[:-3]+"mp3", **{"language": "sv"})
+            result = MODEL.transcribe(path[:-3]+"mp3", **{"language": "sv"})
         else:
-            result = model.transcribe(path, **{"language": "sv"})
+            result = MODEL.transcribe(path, **{"language": "sv"})
 
-        return render_template("success.html", data = result['text'])  
-  
-@app.route('/recieve', methods = ['POST'])  
-def recieve():  
-    if request.method == 'POST':  
-        f = request.files['file']  
-        path = os.path.join(app.config['UPLOAD_FOLDER'], f.filename)
-        f.save(path)
+        return render_template("success.html", data=result['text'])
+
+@APP.route('/recieve', methods=['POST'])
+def recieve():# pylint: disable=inconsistent-return-statements
+    """recieves audiofile and transcribes"""
+    if request.method == 'POST':
+        file = request.files['file']
+        path = os.path.join(APP.config['UPLOAD_FOLDER'], file.filename)
+        file.save(path)
         if path[:-3] == 'wav':
             AudioSegment.from_wav(path).export(path[:-3]+"mp3", format="mp3")
-            result = model.transcribe(path[:-3]+"mp3")
+            result = MODEL.transcribe(path[:-3]+"mp3")
         else:
-            result = model.transcribe(path)
+            result = MODEL.transcribe(path)
 
+        return json.dumps({"data": result['text']})
 
-        return json.dumps({"data": result['text']}) 
-
-if __name__ == '__main__':  
-    app.run(config.IP_ADDRESS, debug = True)  
+if __name__ == '__main__':
+    APP.run(config.IP_ADDRESS, debug=True)
